f02dbd6046f3032833b789e00b79bb18150f3813
Daniel LaCosse <d@niellacos.se>
2014-05-17 23:52:11 -0400

    begins restructuring of textraptor

---
+++ textraptor/lib/dictionary.js
@@ -0,0 +1,19 @@
+var dictionary = {
+  "type": "common words",
+  "pronouns": {
+    "personal": {
+      "singular": new Set([
+        "i", "me", "you", "he", "him", "she", "her", "it"
+      ]),
+      "plural": new Set([
+        "we", "us", "they", "them"
+      ])
+    },
+
+  },
+  "prepositions": {
+
+  }
+
+
+}
---
+++ textraptor/lib/init.js
@@ -0,0 +1,51 @@
+var library = [
+  {avgPcnt: 0, stdDev: 0, color: "#3a320e"},
+  {avgPcnt: 0, stdDev: 0, color: "#6d1a1a"},
+  {avgPcnt: 0, stdDev: 0, color: "#19567a"}
+]
+
+var gaussianCanv = document.getElementById("gaussianCanv");
+var ctx = gaussianCanv.getContext("2d");
+
+var xScale = 0, yScale = 1;
+
+$(function(){
+
+  setupCanvas();
+
+  $(".textarea").on("input propertychange", function(event) {
+    var id = $(event.target).attr("data-id")
+    var text = library[id]
+
+    text.wrdCnt = countWordsFrom(id, text.color);
+    text.avgPcnt = avgPercent(text.wrdCnt);
+    text.stdDev = stdDev(text.wrdCnt);  //average operation duplication
+
+    updateStats();
+    updateGaussianCanv();
+  })
+
+  $("#addText").on("click", function(){
+
+    console.log($("textarea:visible").length);
+
+    if ($("textarea:visible").length === 1) {
+
+      console.log($("textarea:visible").length);
+
+      $(".textarea").transition({height: "210px"}, 300, "ease");
+      $("#area1").slideDown(300);
+
+    } else if ($("textarea:visible").length === 2) {
+
+      console.log($("textarea:visible").length);
+
+      $(".textarea").transition({height: "135px"}, 300, "ease");
+      $("#area2").slideDown(300);
+      $(this).hide();
+
+    };
+
+  });
+
+})
