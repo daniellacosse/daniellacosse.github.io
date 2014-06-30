7ec4ce250eb761a4fe92f1701409e26986397cac
Daniel LaCosse <d@niellacos.se>
2014-06-30 09:43:20 -0400

    moves nal

--- textrepencer/lib/javascripts/dictionary.js
+++ textrepencer/lib/javascripts/dictionary.js
@@ -2,7 +2,7 @@
 
   var nal = root.nal = (root.nal || {});
 
-  nal.Dictionary = Dictionary = function(params){
+  Dictionary = nal.Dictionary = function(params){
     this.language = JSON.serialize( params.language );
   }
 
